a07cb3c0271f6d19905857b2590a1206f5706731
Igor Liakh <igorlyah060@gmail.com>
2023-09-17 00:42:12 +0300

    done task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const LC_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const email = form.elements.email;
+const message = form.elements.message;
+const formData = {};
+
+if (localStorage.getItem(LC_KEY) === null) {
+  email.value = '';
+  message.value = '';
+} else {
+  const values = JSON.parse(localStorage.getItem(LC_KEY));
+  email.value = values.email;
+  message.value = values.message;
+}
+
+const inputHandler = e => {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(LC_KEY, JSON.stringify(formData));
+};
+
+const submitHandler = e => {
+  e.preventDefault();
+  form.reset();
+  localStorage.clear();
+};
+
+form.addEventListener('input', throttle(inputHandler, 500));
+
+form.addEventListener('submit', submitHandler);
