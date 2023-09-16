cf5fb61647031c0540c44b770820baa2c1870a9e
Igor Liakh <igorlyah060@gmail.com>
2023-09-16 18:51:13 +0300

    done task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,16 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { markup } from './markup';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = markup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
