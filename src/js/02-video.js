d7060b022106803ad0aae95eeebb0d8799c81215
Igor Liakh <igorlyah060@gmail.com>
2023-09-17 15:42:29 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const LC_KEY = 'videoplayer-current-time';
+const LS_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
@@ -9,11 +9,11 @@ const onPlay = () => {
   player
     .getCurrentTime()
     .then(seconds => {
-      localStorage.setItem(LC_KEY, JSON.stringify(seconds));
+      localStorage.setItem(LS_KEY, JSON.stringify(seconds));
     })
     .catch(error => console.log(error));
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem(LC_KEY)));
+player.setCurrentTime(JSON.parse(localStorage.getItem(LS_KEY)));
